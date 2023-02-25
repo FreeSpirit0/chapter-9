3b917887113de22eda8dbe5d2240b77eed62ec89
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-02-25 14:52:58 +0700

    Add new service

---
+++ example-1/advertisement/src/fixtures/advertisements.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        name: "Agoda",
+        description: "Book now at Agoda",
+        imageUrl: "https://marketingmagazine.com.my/wp-content/uploads/2019/09/agoda-agoji-resized-scaled.jpg",
+        url: "https://www.agoda.com/"
+    },
+    {
+        name: "Tinder",
+        description: "Dating, Make Friends & Meet New People",
+        imageUrl: "https://tinder.com/static/tinder.png",
+        url: "https://tinder.com/"
+    },
+    {
+        name: "Discord",
+        description: "Your Place to Talk and Hang Out",
+        imageUrl: "https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/62a07b53139aec4c1fd07771_discord-logo.png",
+        url: "https://discord.com"
+    },
+    {
+        name: "Netflix",
+        description: "Watch TV Shows Online, Watch Movies Online",
+        imageUrl: "https://about.netflix.com/images/meta/netflix-symbol-black.png",
+        url: "https://netflix.com"
+    },
+    {
+        name: "Twitter",
+        description: "It's what's happening",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4n_urpJ9XpwOTdzBVbGvactwHrPagYQrTJPYjxfxLGkSyu7nJZVqRVGAeohnPgKMrnKE&usqp=CAU",
+        url: "https://twitter.com"
+    }
+]
---
+++ example-1/advertisement/src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const advertisements = require("./fixtures/advertisements");
+
+function setupHandlers(app) {
+    app.get("/advertisement", (req, res) => {
+        const randomNum = getRandomInt(0, advertisements.length);
+        const slice = advertisements.slice(randomNum);
+        res.send(slice)
+    });
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+//
+// Start the HTTP server.
+//
+function startHttpServer() {
+    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+        const app = express();
+        setupHandlers(app);
+
+        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+        app.listen(port, () => {
+            resolve(); // HTTP server is listening, resolve the promise.
+        });
+    });
+}
+
+//
+// Application entry point.
+//
+function main() {
+    return startHttpServer(); // start the HTTP server.
+}
+
+main()
+    .then(() => console.log("Microservice online."))
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
